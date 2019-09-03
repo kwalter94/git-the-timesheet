2a7a6ab8a5f3047b2ce7ea2cb402786ff1ae0f72
Walter Kaunda <walterkaunda@live.co.uk>
2019-09-03 16:28:47 +0200

    Refactored GitLog class to make usage less verbose.
    - Builder pattern implemented on the class was only adding unnecessary verbosity.

--- git.py
+++ git.py
@@ -6,40 +6,17 @@ class GitLog:
     '''Open git log for a given repository as a file.
     
     Example:
-        >>> GitLog().set_repository(os.getcwd())\
-                    .add_author('walterkaunda@live.co.uk')\
-                    .set_start_date('2019-05-15')\
-                    .set_end_date('2019-05-16')\
-                    .open()\
-                    .readlines()[0]
+        >>> GitLog(repository=os.getcwd(), authors=['walterkaunda@live.co.uk'], start_date='2019-05-15', end_date='2019-05-16')\
+                .open()\
+                .readlines()[0]
         'commit 8c13c3d2d86aaff65211ee2c9c29ee4b7582784a\\n'
     '''
 
-    def __init__(self):
-        self.repository = None
-        self.authors = []
-        self.start_date = None
-        self.end_date = None
-
-    def add_author(self, author):
-        self.authors.append(author)
-
-        return self
-
-    def set_start_date(self, date):
-        self.start_date = date
-
-        return self
-
-    def set_end_date(self, date):
-        self.end_date = date
-        
-        return self
-
-    def set_repository(self, repository):
+    def __init__(self, repository=None, authors=[], start_date=None, end_date=None):
         self.repository = repository
-
-        return self
+        self.authors = authors[:]
+        self.start_date = start_date
+        self.end_date = end_date
 
     def open(self):
         '''Returns a file like object from which the git log can be read.'''
@@ -54,6 +31,7 @@ class GitLog:
         repository = self.repository and f'-C {self.repository}' or ''
 
         return f'git {repository} log {authors} {start_date} {end_date} --date iso --date-order --branches'
+
 
 if __name__ == '__main__':
     import doctest
